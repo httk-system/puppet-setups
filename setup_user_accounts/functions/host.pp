fb23e7b1e491abd8cf29e35e50ce133b94a7a839
Rickard Armiento <rickard-gpg@armiento.net>
2025-07-04 10:05:09 +0000

    Add user accounts subuid/gid support, and nobackup

--- setup_user_accounts/functions/host.pp
+++ setup_user_accounts/functions/host.pp
@@ -4,10 +4,27 @@ function setup_user_accounts::host(
 
       $home = $config['home']
 
+      if $config['nobackup_homes'] {
+        file { "/nobackup":
+          ensure => 'directory',
+          owner  => root,
+          group  => root,
+          mode   => '0755',
+        }    
+      }
+  
       if 'managehome' in $config {
         $managehome = $config['managehome']
       } else {
         $managehome = false
+      }
+
+      $gid_to_groupname = $config['groups'].reduce({}) |$acc, $pair| {
+        $groupname = $pair[0]
+        $groupparams = $pair[1]
+        $acc + {
+          $groupparams['gid'] => $groupname
+        }
       }
       
       $config['groups'].each |$groupname, $groupparams| {
@@ -22,10 +39,13 @@ function setup_user_accounts::host(
 	     $present = 'present'
 	   }
 
+           $gid = $groupparams['gid']
+
            group { $groupname:
 	     ensure => $present,
-	     gid => $groupparams['gid'],
+	     gid => $gid,
 	   }
+
       }
 
       $config['users'].each |$username, $userparams| {
@@ -41,16 +61,52 @@ function setup_user_accounts::host(
 	   }
 
 	   $name = $userparams['name']
-	   
+           $uid = $userparams['uid']
+           $gid = $userparams['gid']
+
            user { $username:
 	     ensure => $present,
 	     comment => "$name,,,",
 	     gid => $userparams['gid'],
 	     home => "$home/$username",
 	     shell => '/bin/bash',
-	     uid => $userparams['uid'],
+	     uid => $uid,
              managehome => $managehome,
 	   }
+
+           if $userparams['subgids'] {
+
+             $subgid_start = $userparams['subgids']
+
+             file_line { "subgid_${username}":
+               ensure => $present,
+               path   => '/etc/subgid',
+               line   => "${username}:${subgid_start}:65536",
+               match  => "^${regexpescape($name)}:",
+             }
+           }
+
+           if $userparams['subuids'] {
+
+             $subuid_start = $userparams['subuids']
+
+             file_line { "subuid_${username}":
+               ensure => $present,
+               path   => '/etc/subuid',
+               line   => "${username}:${subuid_start}:65536",
+               match   => "^${regexpescape($name)}:",
+             }
+           }
+
+           if $config['nobackup_homes'] {
+             file { "/nobackup/${username}":
+               ensure => 'directory',
+               owner  => $username,
+               group  => $gid_to_groupname[$gid],
+               mode   => '0700',
+               require => File['/nobackup'],
+             }
+           }
       }
 
       # Return dependencies
